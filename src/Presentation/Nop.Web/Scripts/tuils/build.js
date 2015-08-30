c6c70878ec9dcb6e6137f1d170f7d3e290f1e5e9
Gabriel Castillo <gabriel.castillo86@hotmail.com>
2015-08-30 07:38:01 -0500

    Se realizan ajustes a las hojas de estilo para la generación automatica de las mismas

--- src/Presentation/Nop.Web/Scripts/tuils/build.js
+++ src/Presentation/Nop.Web/Scripts/tuils/build.js
@@ -5,21 +5,21 @@
     name: 'tuils/main',
     stubModules: ['text'],
     optimizeAllPluginResources: false,
-    findNestedDependencies: false,
+    findNestedDependencies: true,
     //optimize: "none",
     paths: {
         //No agrega al bundle
         maps: 'empty:',
         configuration: 'empty:',
-        resources : 'empty:'
+        resources : 'empty:',
        //, backbone: 'empty:',
        // underscore: 'empty:',
        // stickit: 'empty:',
        // validations: 'empty:',
-       // handlebars: 'empty:',
+        handlebars: 'empty:',
        // accounting: 'empty:',
-       // jquery: 'empty:',
-       // jqueryui: 'empty:',
+        jquery: 'empty:',
+        jqueryui: 'empty:',
        // jqueryunobtrusive: 'empty:',
        // jqueryvalidate: 'empty:',
        // jquerymigrate: 'empty:'
